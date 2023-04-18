40bd276edacf323868af8cb0058d22d4ff7a8312
Kawaeee <kawaekc@gmail.com>
2023-04-18 22:21:21 +0700

    🔨 add streamlit as web application framework

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os
+import pathlib
+import uuid
+
 from PIL import Image
 import numpy as np
 import openpyxl
-import uuid
+import streamlit as st
 
 
-def transform_image_to_xlsx(image_path: str, default_cols: int = 50, is_scale=False) -> None:
+@st.cache_data(persist="disk")
+def transform_image_to_xlsx(image_path: str, default_cols: int = 50, is_rescale=False) -> (str, str):
     """
     Transforms an image into an xlsx file where each cell corresponds to a pixel of the image.
     Each cell is filled with a color corresponding to the RGB value of the corresponding pixel.
@@ -12,14 +17,18 @@ def transform_image_to_xlsx(image_path: str, default_cols: int = 50, is_scale=Fa
     Args:
         image_path (str): The path to the image file.
         default_cols (int, optional): The number of columns to use if `is_scale` is True. Defaults to 50.
-        is_scale (bool, optional): Whether to scale the image to fit `default_cols` columns. Defaults to False.
+        is_rescale (bool, optional): Whether to scale the image to fit `default_cols` columns. Defaults to False.
 
     Returns:
-        None: Saves the xlsx file with a unique filename.
+        file_path (str): xlsx file full path.
+        file_name (str): xlsx file unique file name.
     """
+    # Always check for output directory
+    pathlib.Path("output").mkdir(parents=True, exist_ok=True)
+
     # Open image
     with Image.open(image_path) as image:
-        if is_scale:
+        if is_rescale:
             # Scale image if requested
             width, height = image.size
             image_scale = default_cols / width
@@ -45,5 +54,35 @@ def transform_image_to_xlsx(image_path: str, default_cols: int = 50, is_scale=Fa
                 cell.fill = openpyxl.styles.PatternFill(start_color=fill_color, end_color=fill_color, fill_type="solid")
 
         # Save workbook with unique filename
-        filename = f"{uuid.uuid4().hex[:12]}.xlsx"
-        workbook.save(filename)
+        file_name = f"{uuid.uuid4().hex[:16]}.xlsx"
+        file_path = os.path.join("output", file_name)
+        workbook.save(file_path)
+
+        return (file_path, file_name)
+
+
+if __name__ == "__main__":
+    st.title("ikusel (📷💻📊)")
+    st.write("Simple web-based tool that lets users upload an image and convert it to an Excel spreadsheet (.xlsx) file!")
+
+    uploaded_file = st.file_uploader(label="Upload Image..", type=["png", "jpg", "jpeg"], label_visibility="hidden")
+    left, right = st.columns(2)
+
+    with left:
+        is_rescale = st.checkbox("Rescale", value=True)
+
+        if is_rescale:
+            default_cols = st.number_input("How many columns that would fit your image?", value=50, min_value=25, max_value=200)
+        else:
+            default_cols = 0
+            is_rescale = False
+
+    if uploaded_file is not None:
+        with left:
+            submit_button = st.button(label="Submit")
+            if submit_button:
+                file_path, file_name = transform_image_to_xlsx(uploaded_file, default_cols, is_rescale)
+                with open(file_path, "rb") as f:
+                    download_button = st.download_button(label="Download", data=f, file_name=file_name)
+        with right:
+            st.image(uploaded_file, use_column_width="auto", caption="User uploaded image")
